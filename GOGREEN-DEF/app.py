69a183629550f59603c6b7bbadb149564a49e4e4
Allepinzu <axelpinzuti@gmial.com>
2021-12-18 18:01:48 +0100

    Cenni di db2

--- GOGREEN-DEF/app.py
+++ GOGREEN-DEF/app.py
@@ -1,12 +1,25 @@
 from flask import Flask, render_template, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
-import os
+from sqlalchemy.testing.pickleable import User
+
 from form import LoginForm
+
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'hard to guess'
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///website.db'
-db=SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///website.db"
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
+db = SQLAlchemy(app)
+
+
+@app.before_first_request
+def create_db():
+
+    db.create_all()
+    name = User(name='name')
+
+
 
 @app.route('/')
 def homepage():  # put application's code here
@@ -14,25 +27,23 @@ def homepage():  # put application's code here
     return render_template("index.html", username=username)
 
 
-@app.route('/login',methods=['POST','GET'])
+@app.route('/login', methods=['POST', 'GET'])
 def login_page():
-    password=None
-    username=None
+    password = None
+    username = None
     form = LoginForm()
     if form.validate_on_submit():
         username = form.username.data
         password = form.password.data
         session['username'] = username
         return redirect(url_for('homepage'))
-    return render_template('login.html',form=form, username=username, password=password)
+    return render_template('login.html', form=form, username=username, password=password)
 
 
-@app.route('/logout',methods=['POST','GET'])
+@app.route('/logout', methods=['POST', 'GET'])
 def logout_page():
     session.clear()
     return redirect(url_for('homepage'))
-
-
 
 
 if __name__ == '__main__':
