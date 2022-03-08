84d673c14899ac75da3bbbf7f36e2e7210c39861
filippomattio <s257509@studenti.polito.it>
2022-03-08 11:59:40 +0100

    error handler

--- GOGREEN-DEF/app.py
+++ GOGREEN-DEF/app.py
@@ -680,3 +680,13 @@ def foot():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+@app.errorhandler(404)
+def error404(error):
+    return render_template('404.html'), 404
+@app.errorhandler(403)
+def error403(error):
+    return render_template('403.html'), 403
+@app.errorhandler(500)
+def error500(error):
+    return render_template('500.html'), 500
